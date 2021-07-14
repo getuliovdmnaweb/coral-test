2994eca33315822b941150083b9d3114539bc2c8
Getulio Vieira Desktop <getuliovdmnaweb@gmail.com>
2021-07-14 15:05:14 -0300

    Adding React Navigation

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 import { client } from "./src/api";
-import { OverviewScreen } from "./src/screens";
 import { SafeArea } from "./src/components/SafeArea";
 import { theme } from "./src/theme";
 import { ThemeProvider } from "./src/theme/styled-components";
+import { MainNavigator } from "./src/navigator";
 
 export default function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <SafeArea>
-          <ApolloProvider client={client}>
-            <OverviewScreen />
-          </ApolloProvider>
-        </SafeArea>
-      </ThemeProvider>
+      <NavigationContainer>
+        <ThemeProvider theme={theme}>
+          <SafeArea>
+            <ApolloProvider client={client}>
+              <MainNavigator />
+            </ApolloProvider>
+          </SafeArea>
+        </ThemeProvider>
+      </NavigationContainer>
+
       <ExpoStatusBar style="dark" />
     </>
   );
